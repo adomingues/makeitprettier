11eb48afffac66531e49ef3e6678f9f890d527a4
Antonio Domingues <amjdomingues@gmail.com>
2018-03-04 17:13:13 +0100

    first commit

--- R/colors.R
+++ R/colors.R
@@ -1,25 +1,117 @@
-#' Gives some color to plots
+pretty_colors <- c(
+  `blue` = "#386cb0",
+  `orange` = "#fdb462",
+  `green` = "#7fc97f",
+  `red` = "#ef3b2c",
+  `brown` = "#662506",
+  `light blue` = "#a6cee3",
+  `salmon` = "#fb9a99",
+  `purple` = "#984ea3",
+  `yellow` = "#ffff33"
+)
+
+#' Function to extract pretty colors as hex codes
 #'
-#' A color scheme for plots that is both colorblind-friendly
-#' and yet is still aesthetically pleasing
+#' source:
+#' https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
 #'
+#' @param ... Character names of pretty_colors
 #'
 #' @export
-#' @return A color scale for ggplot
-scale_fill_ketting <- function(...){
-      discrete_scale("fill","ketting",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
-      
+prettier_cols <- function(...) {
+  cols <- c(...)
+
+  if (is.null(cols)) {
+    return(pretty_colors)
+  }
+
+  pretty_colors[cols]
 }
 
-#' Gives some color to plots
+
+prettier_palettes <- list(
+  `main` = prettier_cols(),
+  `cool` = prettier_cols("blue", "green"),
+  `hot` = prettier_cols("yellow", "orange", "red"),
+  `mixed` = prettier_cols("blue", "green", "yellow", "orange", "red")
+)
+
+
+#' Return function to interpolate a prettier color palette
 #'
-#' A color scheme for plots that is both colorblind-friendly
-#' and yet is still aesthetically pleasing
+#' @param palette Character name of palette in prettier_palettes
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments to pass to colorRampPalette()
 #'
+prettier_pal <- function(palette = "main", reverse = FALSE, ...) {
+  pal <- prettier_palettes[[palette]]
+
+  if (reverse) pal <- rev(pal)
+
+  colorRampPalette(pal, ...)
+}
+
+
+#' Color scale constructor for drsimonj colors
+#'
+#' @param palette Character name of palette in drsimonj_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_color_drsimonj <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- prettier_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    discrete_scale("colour", paste0("drsimonj_", palette), palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+
+#' Fill scale constructor for drsimonj colors
+#'
+#' @param palette Character name of palette in drsimonj_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_fill_drsimonj <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- prettier_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    discrete_scale("fill", paste0("drsimonj_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
+
+
+
+#' Color scale constructor for prettier colors
+#'
+#' @param palette Character name of palette in prettier_palettes
+#' @param ... Unused for now
 #'
 #' @export
-#' @return A color scale for ggplot
-scale_colour_ketting <- function(...){
-      discrete_scale("colour","ketting",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
-      
+scale_color_prettier <- function(palette = "main", ...) {
+  pal <- prettier_palettes[[palette]]
+
+  discrete_scale("colour", paste0("prettier_", palette), manual_pal(values = unname(pal)), ...)
+}
+
+
+#' Fill scale constructor for prettier colors
+#'
+#' @param palette Character name of palette in prettier_palettes
+#' @param ... Unused for now
+#'
+#' @export
+scale_fill_prettier <- function(palette = "main", ...) {
+  pal <- prettier_palettes[[palette]]
+
+  discrete_scale("fill", paste0("prettier_", palette), manual_pal(values = unname(pal)), ...)
 }
--- README.md
+++ README.md
@@ -4,51 +4,46 @@ The goal of makeitprettier is to create a set of visual schemes to:
 1. generate visually consistent plots for the lab
 2. make it easier for lab members to tweak plots for presentations, reports and publication.
 
-## Example
 
-This is a basic example which shows you how to use the functions:
+## Installation
 
-First create a fake dataset:
+The easiest way is using `devtools` to install straight from github:
 
 ```r
-set.seed(123)
-counts <- data.frame(IP = rep(c("ProtA", "ProtB"), 60),
-    Tissue = rep(c("Testis", "Brain"), 60),
-    Length = rep(c(21:35), 4),
-    RPM = c(
-        rnorm(30, mean = 100, sd = 20),
-        rnorm(30, mean = 50, sd = 10)
-        )
-    )    
-
-counts$Sample <- paste(counts$Tissue, counts$IP, sep="_")
-counts
+# install.packages("devtools")
+devtools::install_github("adomingues/makeitprettier")
 ```
 
 
-``` r
-library(ggplot2)
-## basic example code
-p_ex <- ggplot(counts, aes(x = Length, y = RPM, color = Sample)) +
-    geom_line() + 
-    facet_wrap(~ Sample, ncol=1)
-p_ex
+## Example
+
+See the vignette for full examples by running:
+
+```r
+browseVignettes("makeitprettier")
 ```
 
-Now we can use the themes by adding the desired function:
+Very quickly, to add a theme to your plot:
 
 ```r
 library(makeitprettier)
-p_ex + theme_poster()
+p1 <- ggplot(readcounts, aes(x = readlength, y = N, color = Mapping)) +
+  geom_line() +
+  facet_grid(IP ~ Replicate)
+p1
+p1 + theme_poster()
 ```
 
-The default colors are still on the meh side of life, so let's perk things up:
+And change the colors with:
 
 ```r
-p_ex + 
+p1 + 
     theme_poster() +
     scale_colour_ketting()
 ```
+
+
+## Sources
 
 I took inspiration and or used the code from:
 [ggthemes](https://github.com/jrnold/ggthemes)
