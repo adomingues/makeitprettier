bfcf990ca309e49a38d4c81b6ab3f1988ea332b7
Antonio Domingues <amjdomingues@gmail.com>
2018-03-04 22:06:03 +0100

    Added function to save data

--- R/export_plot.R
+++ R/export_plot.R
@@ -1,3 +1,24 @@
+#' Saves data underlying a ggplot
+#'
+#' Helper function that saves the data used to create a gglot.
+#' This should help reproducing plots.
+#'
+#' @param filename Base file name/path
+#' @param plot = last_plot() The ggplot for which the data will be saved
+#'
+#' @export
+#' @return a file is saved
+save_data <- function(filename, plot = last_plot()) {
+   dat <- plot$data
+   write.table(
+      dat,
+      file = filename,
+      quote = FALSE,
+      sep = "\t",
+      row.names = FALSE
+   )
+}
+
 #' Saves plots
 #'
 #' Helper function that saves (gg)plots as PNG and PDF simultaneously
@@ -8,12 +29,20 @@
 #' @param filename Base file name/path
 #' @param width width of the save image. Default: 15 cm
 #' @param height height of the save image. Default: 15 cm
+#' @param save_data whether to save the data underlying the plot. The filename will be a composite of _filename_ and ".data.txt"
+#' @param ... Additional ggsave arguments
 #'
 #' @export
-#' @return A file is saved
-
-save_poster <- function(plot, filename, width=15, height=15, units = "cm") {
+#' @return Files are saved
+save_plot <- function(filename, plot = last_plot(), width=15, height=15, units = "cm", save_data = FALSE, ...) {
   # Export plot in PDF and PNG
-  ggsave(paste(filename, ".pdf", sep = ""), plot, width = width, height = height)
-  ggsave(paste(filename, ".png", sep = ""), plot, width = width, height = height)
+  ggsave(paste(filename, ".pdf", sep = ""), plot, width = width, height = height, ...)
+  ggsave(paste(filename, ".png", sep = ""), plot, width = width, height = height, ...)
+
+  if (save_data == TRUE){
+   data_file <- paste0(filename, ".data.txt")
+   save_data(data_file)
+  }
 }
+
+
